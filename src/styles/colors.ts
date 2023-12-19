4c7cf9fc839a9baa778a81d362f362b7c4cf9fad
Eduardo Castro <eduardo.castro@g.globo>
2023-12-19 11:59:55 -0300

    chore: setup colors file

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,1 +1,16 @@
-export const COLORS = {}
+export const COLORS = {
+  BORDER: {
+    GRAY: '#454545',
+    BLUE: '#3767CC'
+  },
+  ICON: {
+    GRAY: '#BCBCBC',
+    WHITE: '#FFF'
+  },
+  BACKGROUND: {
+    BLACK: '#0F0F0F',
+    LIGHT_BLACK: '#252525',
+    GRAY: '#656565',
+    TRANSPARENT_GRAY: 'rgba(200, 200, 200, 0.5)'
+  }
+}
